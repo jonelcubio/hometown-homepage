61547831de103690161e2c978d567deaf08e6a58
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-12-09 16:05:20 +0800

    🌸 subtitle added

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,7 +6,9 @@ export function Landing() {
         <img src="https://images.pexels.com/photos/2848492/pexels-photo-2848492.jpeg" className="img" alt="Seoul, Seoul, South Korea" title="Seoul, Seoul, South Korea" />
       </div>
 
-      <div className="heading">Seoul,South Korea</div>
+      <a href="https://github.com/jonelcubio/hometown-homepage" target="_blank" rel="noreferrer"><div className="heading">Seoul,South Korea</div></a>
+
+      <div className="subtitle">Enjoy culture and sports in this vibrant city close to the center of Seoul.</div>
     </>
   );
 }
